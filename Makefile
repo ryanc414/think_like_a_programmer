c51dc7a14d55214203f8090ba46b40cd33019c05
Ryan Collingham <ryanc118@gmail.com>
2019-01-31 23:38:54 +0000

    WIP: p2_words

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 .PHONY: all, clean
 
 CC=g++
-CFLAGS=-Wall -Werror
+CFLAGS=-Wall -Werror -Og
 
-all: bin/p2_hashes bin/p2_isbn bin/p2_binary
+all: bin/p2_hashes bin/p2_isbn bin/p2_binary bin/p2_words
 
 clean:
 	rm -rf bin/*
---
+++ src/p2_words.cc
@@ -0,0 +1,87 @@
+/* 2-9. Write a program that reads a line of text, counting the number of
+ * words, identifying the length of the longest word, the greatest number of
+ * vowels in a word, and/or any other statistics you can think of. */
+
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int count_words(string line);
+int longest_word(string line);
+inline bool is_letter(char c);
+
+
+int main()
+{
+    string line;
+    int word_count;
+    int longest_word_len;
+
+    //cout << "Enter a line:" << endl << "> ";
+    //cin >> line;
+    //cout << line << endl;
+
+    line = "test string here.";
+
+    word_count = count_words(line);
+    longest_word_len = longest_word(line);
+
+    cout << "Word count: " << word_count << endl;
+    cout << "Longest word length: " << longest_word_len << endl;
+
+    return 0;
+}
+
+// Count the number of words in a string. Words are consecutive blocks of
+// letters.
+int count_words(string line)
+{
+    int count = 0;
+    bool in_word = false;
+
+    for (char &c : line) {
+        if (is_letter(c)) {
+            in_word = true;
+        } else if (in_word) {
+            count++;
+            in_word = false;
+        }
+    }
+
+    if (in_word) {
+        count++;
+    }
+
+    return count;
+}
+
+// Return the length of the longest word within a string.
+int longest_word(string line)
+{
+    int longest_length = 0;
+    int curr_length = 0;
+
+    for (char &c : line) {
+        if (is_letter(c)) {
+            curr_length++;
+        } else {
+            curr_length = 0;
+        }
+
+        if (curr_length > longest_length) {
+            longest_length = curr_length;
+        }
+    }
+
+    return longest_length;
+}
+
+inline bool is_letter(char c)
+{
+    return c >= 'a' && c <= 'Z';
+}
+
