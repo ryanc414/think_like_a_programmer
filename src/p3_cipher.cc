1352a1c8660f0c7b63d0200bdd4661da7fd886ba
Ryan Collingham <ryanc118@gmail.com>
2019-02-23 18:44:58 +0000

    Add decrypt function and CipherAccess class helper

--- src/p3_cipher.cc
+++ src/p3_cipher.cc
@@ -5,6 +5,9 @@
  * another letter (for example, each C could become an X). For this problem,
  * hard-code a const array of 26 char elements for the cipher, and have your
  * program read a plaintext message and output the equivalent ciphertext.
+ *
+ * Have the previous program convert the ciphertext back to the plaintext to
+ * verify the encoding and decoding.
  */
 
 #include <iostream>
@@ -14,12 +17,48 @@
 using std::cout;
 using std::cin;
 using std::endl;
+using std::getline;
 using std::array;
 using std::string;
 
-const string encrypt_plaintext(const string &plaintext);
+void encrypt_user_message();
+void decrypt_user_message();
+const string encrypt_string(const string &plaintext);
+const string decrypt_string(const string &encrypted);
 
-array<char,26> CIPHER = {
+const int NUM_LETTERS = 26;
+
+// Class that gives easy access to a cipher array via operator[]
+//
+// Usage e.g: cipher['G'] gives cipher value for letter G.
+class CipherAccess {
+  public:
+    // Construct from a cipher array.
+    explicit CipherAccess(const array<char, NUM_LETTERS> cipher_array)
+        : cipher_array_(cipher_array) {};
+
+    // operator[] provides read-access to the cipher value for a letter.
+    const char &operator[](const char letter) const {
+        return cipher_array_[letter - 'A'];
+    }
+
+    // Construct a new cipher that inverts this cipher.
+    CipherAccess inverse() const {
+        array<char, NUM_LETTERS> inverse_arr;
+
+        for (char c = 'A'; c < 'Z'; ++c) {
+            inverse_arr[cipher_array_[c - 'A'] - 'A'] = c;
+        }
+
+        return CipherAccess(inverse_arr);
+    }
+
+  private:
+    const array<char, NUM_LETTERS> cipher_array_;
+};
+
+
+const CipherAccess CIPHER({
     'G',
     'H',
     'I',
@@ -46,34 +85,84 @@ array<char,26> CIPHER = {
     'D',
     'E',
     'F'
-};
+});
+
+const CipherAccess INVERSE_CIPHER = CIPHER.inverse();
 
 
+// Either encrypt or decrypt a user's message.
 int main() {
-    string plaintext;
+    string rsp;
 
-    cout << "Enter the plaintext:" << endl << " >";
-    cin >> plaintext;
+    cout << "(E)ncrypt or (D)ecrypt a message?" << endl << " > ";
+    getline(cin, rsp);
 
-    const string encrypted = encrypt_plaintext(plaintext);
-    cout << "Encrypted: " << encrypted << endl;
+    if (rsp[0] == 'E' || rsp[0] == 'e') {
+        cout << "Encrypting a message." << endl;
+        encrypt_user_message();
+    } else if (rsp[0] == 'D' || rsp[0] == 'd') {
+        cout << "Decrypting a message." << endl;
+        decrypt_user_message();
+    }
 
     return 0;
 }
 
 
-const string encrypt_plaintext(const string &plaintext) {
+// Read in a line of plaintext and output the encrypted line.
+void encrypt_user_message() {
+    string plaintext;
+
+    cout << "Enter the plaintext line:" << endl << " > ";
+    getline(cin, plaintext);
+
+    const string encrypted = encrypt_string(plaintext);
+    cout << "Encrypted: " << encrypted << endl;
+}
+
+
+// Read in a line containing an encrypted message and output the decrypted
+// plaintext.
+void decrypt_user_message() {
+    string encrypted;
+
+    cout << "Enter the encrypted line:" << endl << " > ";
+    getline(cin, encrypted);
+
+    const string plaintext = decrypt_string(encrypted);
+    cout << "Plaintext: " << plaintext << endl;
+}
+
+// Convert a plaintext string into an encrypted string using the cipher.
+const string encrypt_string(const string &plaintext) {
     string encrypted;
     encrypted.reserve(plaintext.size());
 
     for (char c : plaintext) {
         if (c >= 'A' && c <= 'Z') {
-            encrypted.push_back(CIPHER[c - 'A']);
+            encrypted.push_back(CIPHER[c]);
         } else {
             encrypted.push_back(c);
         }
     }
 
     return encrypted;
+}
+
+
+// Convert an encrypted string into its decrypted plaintext.
+const string decrypt_string(const string &encrypted) {
+    string decrypted;
+    decrypted.reserve(encrypted.size());
+
+    for (char c : encrypted) {
+        if (c >= 'A' && c <= 'Z') {
+            decrypted.push_back(INVERSE_CIPHER[c]);
+        } else {
+            decrypted.push_back(c);
+        }
+    }
+
+    return decrypted;
 }
 
