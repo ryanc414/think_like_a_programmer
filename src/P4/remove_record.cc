b88d9ede0c57b7030eb3bc769c2bd5582146fa27
Ryan Collingham <ryanc118@gmail.com>
2019-03-28 22:25:58 +0000

    WIP: convert StudentCollection into a class

--- src/P4/remove_record.cc
+++ src/P4/remove_record.cc
@@ -2,84 +2,43 @@
  * 4-5. Write a function removeRecord that takes a pointer to a studentCollection and a
  * student number and that removes the record with that student number from
  * the collection.
+ *
+ * 5-7. Take the student record functions from Chapter 4 (addRecord and averageRecord)
+ * and use them to create a class representing a collection of student records, as
+ * before, making sure to implement all necessary constructors, a destructor,
+ * and an overloaded assignment operator.
  */
 
 #include <iostream>
 #include <cassert>
 
-struct ListNode {
-    int student_num;
-    int grade;
-    ListNode *next;
-};
+class StudentCollection {
+  public:
+    // Constructors and destructor.
+    StudentCollection() = default;
+    StudentCollection(const StudentCollection &other);
+    StudentCollection(StudentCollection &&other);
+    ~StudentCollection();
 
-struct StudentCollection {
-    ListNode *head;
+    // Overloaded operators.
+    StudentCollection & operator=(const StudentCollection &other);
+    StudentCollection & operator=(StudentCollection &&other);
 
-    // Construct an empty StudentCollection.
-    StudentCollection() {
-        head = nullptr;
-    }
+    // Other public member functions.
+    void AddRecord(int student_num, int grade);
+    bool RemoveRecord(int student_num);
+    int CountRecords() const;
+    double AverageRecords() const;
 
-    // Free all student records on desctruction.
-    ~StudentCollection() {
-        ListNode *record = head;
-        ListNode *next_record;
+  private:
+    struct ListNode {
+        int student_num;
+        int grade;
+        ListNode *next;
+    } *head_;
 
-        while (record != nullptr) {
-            next_record = record->next;
-            delete record;
-            record = next_record;
-        }
-    }
-
-    // Adds a new record to the head of the list.
-    void AddRecord(int student_num, int grade) {
-        ListNode *new_record = new ListNode{student_num, grade, head};
-        head = new_record;
-    }
-
-    // Remove a record that matches the given student num. Returns true if the
-    // record was found and deleted, false otherwise.
-    bool RemoveRecord(int student_num) {
-        ListNode *record = head;
-        ListNode *prev_record = nullptr;
-
-        // Loop through the list of students looking for a matching
-        // student_num.
-        while (record != nullptr) {
-            if (record->student_num == student_num) {
-                // Remove the matching record from the list and delete it.
-                if (prev_record == nullptr) {
-                    head = record->next;
-                } else {
-                    prev_record->next = record->next;
-                }
-
-                delete record;
-                return true;
-            }
-
-            prev_record = record;
-            record = record->next;
-        }
-
-        return false;
-    }
-
-    // Count the number of records in the collection.
-    int CountRecords() const {
-        ListNode *record = head;
-        int count = 0;
-
-        while (record != nullptr) {
-            ++count;
-            record = record->next;
-        }
-
-        return count;
-    }
-
+    void DeleteList();
+    ListNode *CopyList(ListNode *from);
 };
 
 int main() {
@@ -109,4 +68,99 @@ int main() {
 
     return 0;
 }
+
+// Copy constructor.
+StudentCollection::StudentCollection(const StudentCollection &other) {
+    for (ListNode *record = other.head_;
+         record != nullptr;
+         record = record->next) {
+        AddRecord(record->student_num, record->grade);
+    }
+}
+
+// Move constructor.
+StudentCollection::StudentCollection(StudentCollection &&other) {
+    head_ = other.head_;
+    other.head_ = nullptr;
+}
+
+// Destructor.
+StudentCollection::~StudentCollection() {
+    ListNode *record = head;
+    ListNode *next_record;
+
+    while (record != nullptr) {
+        next_record = record->next;
+        delete record;
+        record = next_record;
+    }
+}
+
+// Copy assignment.
+StudentCollection & operator=(const StudentCollection &other) {
+    if (*this != other) {
+        DeleteList();
+        CopyList(other.head_);
+    }
+
+    return *this;
+
+}
+
+// Adds a new record to the head of the list.
+void StudentCollection::AddRecord(int student_num, int grade) {
+    ListNode *new_record = new ListNode{student_num, grade, head};
+    head = new_record;
+}
+
+// Remove a record that matches the given student num. Returns true if the
+// record was found and deleted, false otherwise.
+bool StudentCollection::RemoveRecord(int student_num) {
+    ListNode *record = head;
+    ListNode *prev_record = nullptr;
+
+    // Loop through the list of students looking for a matching
+    // student_num.
+    while (record != nullptr) {
+        if (record->student_num == student_num) {
+            // Remove the matching record from the list and delete it.
+            if (prev_record == nullptr) {
+                head = record->next;
+            } else {
+                prev_record->next = record->next;
+            }
+
+            delete record;
+            return true;
+        }
+
+        prev_record = record;
+        record = record->next;
+    }
+
+    return false;
+}
+
+// Count the number of records in the collection.
+int StudentCollection::CountRecords() const {
+    ListNode *record = head;
+    int count = 0;
+
+    while (record != nullptr) {
+        ++count;
+        record = record->next;
+    }
+
+    return count;
+}
+
+void StudentCollection::DeleteList(ListNode *) {
+    ListNode *record = head;
+    ListNode *next_record;
+
+    while (record != nullptr) {
+        next_record = record->next;
+        delete record;
+        record = next_record;
+    }
 
