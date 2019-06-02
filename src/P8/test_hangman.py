cf78206522d860e24d24f8fb1aec505ecdb91dc8
Ryan Collingham <ryanc118@gmail.com>
2019-06-02 13:51:58 +0100

    Add test for hangman guesser

--- src/P8/test_hangman.py
+++ src/P8/test_hangman.py
@@ -7,17 +7,26 @@ import pexpect
 
 ROOT_DIR = os.path.abspath(os.path.join(
     os.path.dirname(__file__), os.pardir, os.pardir))
-HANGMAN_BIN = os.path.join(ROOT_DIR, 'bin', 'P8', 'cheating_hangman')
+CHEATING_HANGMAN_BIN = os.path.join(ROOT_DIR, 'bin', 'P8', 'cheating_hangman')
+HANGMAN_GUESSER_BIN = os.path.join(ROOT_DIR, 'bin', 'P8', 'hangman_guesser')
 
 
 def main():
+    """Test all hangman programs."""
+    test_cheating_hangman()
+    test_hangman_guesser()
+
+    print('Hangman test passed')
+
+
+def test_cheating_hangman():
     """
     Test the hangman game from the CLI.
 
     Send in the letters from a-u - the word "outsport" should be guessed.
     """
     child = pexpect.spawn('{bin} {word_len} {num_guesses}'.format(
-        bin=HANGMAN_BIN, word_len=8, num_guesses=26))
+        bin=CHEATING_HANGMAN_BIN, word_len=8, num_guesses=26))
     child.expect('Guess a letter:\r\n> ')
 
     # Guess the letters in order from a to t.
@@ -32,7 +41,20 @@ def main():
     child.expect('Congratulations - you guessed the word!\r\n'
                  'Word was: outsport\r\n')
 
-    print('Hangman test passed')
+
+def test_hangman_guesser():
+    """
+    Test the hangman guesser.
+
+    It should be able to guess the word "cannibalism" with fewer than 10
+    misses.
+    """
+    child = pexpect.spawn('{bin} {num_guesses}'.format(
+        bin=HANGMAN_GUESSER_BIN, num_guesses=10))
+
+    child.expect('Enter a word for me to guess: \r\n> ')
+    child.sendline('cannibalism')
+    child.expect('I guessed your word was cannibalism', timeout=60)
 
 
 if __name__ == '__main__':
