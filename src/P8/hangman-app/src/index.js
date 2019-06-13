b6062ff4ea9f9886bd43c9cd947c8a83a1b6615c
Ryan Collingham <ryanc118@gmail.com>
2019-06-13 01:11:14 +0100

    Correctly import and use images

--- src/P8/hangman-app/src/index.js
+++ src/P8/hangman-app/src/index.js
@@ -2,17 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import hangman0 from './hangman0.png'
+import hangman1 from './hangman1.png'
+import hangman2 from './hangman2.png'
+import hangman3 from './hangman3.png'
+import hangman4 from './hangman4.png'
+import hangman5 from './hangman5.png'
+import hangman6 from './hangman6.png'
+import hangman7 from './hangman7.png'
+import hangman8 from './hangman8.png'
+import hangman9 from './hangman9.png'
+import hangman10 from './hangman10.png'
+import hangman11 from './hangman11.png'
+import hangman12 from './hangman12.png'
+import hangman13 from './hangman13.png'
+
+const hangman_images = [
+  hangman0,
+  hangman1,
+  hangman2,
+  hangman3,
+  hangman4,
+  hangman5,
+  hangman6,
+  hangman7,
+  hangman8,
+  hangman9,
+  hangman10,
+  hangman11,
+  hangman12,
+  hangman13,
+]
+
+// Selects and renders a hangman image depending on the number of misses.
 function Hangman(props) {
+  const img_src = hangman_images[props.misses]
   return (
-    <img src={"hangman" + props.misses + ".png"}
-         alt={"A poor stick man in step "
-              + props.misses
-              + " of being hanged :("}
+    <img
+      src={img_src}
+      alt={
+        "A poor stick man in stage "
+        + props.misses
+        + " of being hanged :("
+      }
     />
   );
 }
 
-
+// Top-level game component.
 class Game extends React.PureComponent {
   constructor(props) {
     super(props)
